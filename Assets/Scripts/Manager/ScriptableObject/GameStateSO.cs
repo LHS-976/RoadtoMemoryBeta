70857b1d58f94adaab65dc883af55d7bc6d1cd1b
LHS-976 <showmename@gmail.com>
2026-02-04 17:08:08 +0900

    add

--- Assets/Scripts/Manager/ScriptableObject/GameStateSO.cs
+++ Assets/Scripts/Manager/ScriptableObject/GameStateSO.cs
@@ -15,16 +15,6 @@ namespace Core
     [CreateAssetMenu(fileName = "New GameState", menuName = "InGameState/GameState Data")]
     public class GameStateSO : ScriptableObject
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+       
     }
 }
